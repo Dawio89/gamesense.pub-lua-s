3370a6dbbdfe30a864709166a833ad2a504a44a6
Dawio89 <110141728+Dawio89@users.noreply.github.com>
2023-09-05 14:33:56 +0200

    Update [D] Nazi crosshair.lua
    -added size slider
    -optimized

--- [D] Nazi crosshair.lua
+++ [D] Nazi crosshair.lua
@@ -1,64 +1,70 @@
 --omg crash no way
 --lua tab -> A -> Nazi crosshair
 
+-- local variables for API functions. any changes to the line below will be lost on re-generation
+local client_screen_size, client_set_event_callback, entity_get_local_player, globals_realtime, math_atan, math_cos, math_floor, math_sin, renderer_line, table_insert, ui_get, ui_new_checkbox, ui_new_color_picker, ui_new_slider = client.screen_size, client.set_event_callback, entity.get_local_player, globals.realtime, math.atan, math.cos, math.floor, math.sin, renderer.line, table.insert, ui.get, ui.new_checkbox, ui.new_color_picker, ui.new_slider
+
 local vector = require 'vector';
 
-local screenWidth, screenHeight = client.screen_size()
+local screenWidth, screenHeight = client_screen_size()
 local screen = vector(screenWidth, screenHeight, nil)
 local Middle = vector(screen.x * .5, screen.y * .5, nil)
 local rotation = 0
 
-local crosshairEnable = ui.new_checkbox("Lua", "A", "Nazi crosshair")
-local crosshairColor = ui.new_color_picker("Lua", "A", "Nazi crosshair color", 200, 200, 200, 255)
-local crosshairRainbow = ui.new_checkbox("Lua", "A", "Nazi crosshair rainbow")
+local crosshairEnable = ui_new_checkbox("Lua", "A", "Nazi crosshair")
+local crosshairColor = ui_new_color_picker("Lua", "A", "Nazi crosshair color", 200, 200, 200, 255)
+local crosshairRainbow = ui_new_checkbox("Lua", "A", "Nazi crosshair rainbow")
+local crosshiarSize = ui_new_slider("Lua", "A", "Nazi crosshair size", 1, 100, 10)
 
 local function BOG_TO_GRD(BOG)
-    return (180 / math.pi) * BOG
+	return (180 / math.pi) * BOG
 end
 
 local function GRD_TO_BOG(GRD)
-    return (math.pi / 180) * GRD
+	return (math.pi / 180) * GRD
 end
 
 local function SwastikaCrosshair()
-    pLocal = entity.get_local_player()
-    if not pLocal then return end
+	pLocal = entity_get_local_player()
+	if not pLocal then return end
 
-    if ui.get(crosshairEnable) then
-        local crosshairColorRed, crosshairColorGreen, crosshairColorBlue = ui.get(crosshairColor)
+	if ui_get(crosshairEnable) then
+		local crosshairColorRed, crosshairColorGreen, crosshairColorBlue = ui_get(crosshairColor)
 
-            if rotation < 90 then 
-                rotation = rotation + 1
-            end
-            if rotation > 89 then
-                rotation = 0
-            end
+		if rotation < 90 then
+			rotation = rotation + 1
+		end
+		if rotation > 89 then
+			rotation = 0
+		end
 
-        local a = math.floor(screen.y / 2 / 30)
-        local gamma = math.atan(a / a)
-        
-        for i = 0, 3 do
-            local p = {}
-            table.insert(p, a * math.sin(GRD_TO_BOG(rotation + (i * 90))))
-            table.insert(p, a * math.cos(GRD_TO_BOG(rotation + (i * 90))))
-            table.insert(p, (a / math.cos(gamma)) * math.sin(GRD_TO_BOG(rotation + (i * 90) + BOG_TO_GRD(gamma))))
-            table.insert(p, (a / math.cos(gamma)) * math.cos(GRD_TO_BOG(rotation + (i * 90) + BOG_TO_GRD(gamma))))
-            if not ui.get(crosshairRainbow) then
-                renderer.line(Middle.x, Middle.y, Middle.x + p[1], Middle.y - p[2], crosshairColorRed, crosshairColorGreen, crosshairColorBlue, 255)
-                renderer.line(Middle.x + p[1], Middle.y - p[2], Middle.x + p[3], Middle.y - p[4], crosshairColorRed, crosshairColorGreen, crosshairColorBlue, 255)
-            else
-                r = math.floor(math.sin(globals.realtime() * 2) * 127 + 128)
-                g = math.floor(math.sin(globals.realtime() * 2 + 2) * 127 + 128)
-                b = math.floor(math.sin(globals.realtime() * 2 + 4) * 127 + 128)
-                renderer.line(Middle.x, Middle.y, Middle.x + p[1], Middle.y - p[2], r, g, b, 255)
-                renderer.line(Middle.x + p[1], Middle.y - p[2], Middle.x + p[3], Middle.y - p[4], r, g, b, 255)
-            end
-        end
-    end
+		local crosshairSize = ui_get(crosshiarSize)*.1
+		local a = math_floor(screen.y / 2 / 30) * crosshairSize  -- Adjust the crosshair size here
+		local gamma = math_atan(a / a)
+
+		for i = 0, 3 do
+			local p = {}
+			table_insert(p, a * math_sin(GRD_TO_BOG(rotation + (i * 90))))
+			table_insert(p, a * math_cos(GRD_TO_BOG(rotation + (i * 90))))
+			table_insert(p, (a / math_cos(gamma)) * math_sin(GRD_TO_BOG(rotation + (i * 90) + BOG_TO_GRD(gamma))))
+			table_insert(p, (a / math_cos(gamma)) * math_cos(GRD_TO_BOG(rotation + (i * 90) + BOG_TO_GRD(gamma))))
+			if not ui_get(crosshairRainbow) then
+				renderer_line(Middle.x, Middle.y, Middle.x + p[1], Middle.y - p[2], crosshairColorRed, crosshairColorGreen, crosshairColorBlue, 255)
+				renderer_line(Middle.x + p[1], Middle.y - p[2], Middle.x + p[3], Middle.y - p[4], crosshairColorRed, crosshairColorGreen, crosshairColorBlue, 255)
+			else
+				r = math_floor(math_sin(globals_realtime() * 2) * 127 + 128)
+				g = math_floor(math_sin(globals_realtime() * 2 + 2) * 127 + 128)
+				b = math_floor(math_sin(globals_realtime() * 2 + 4) * 127 + 128)
+				renderer_line(Middle.x, Middle.y, Middle.x + p[1], Middle.y - p[2], r, g, b, 255)
+				renderer_line(Middle.x + p[1], Middle.y - p[2], Middle.x + p[3], Middle.y - p[4], r, g, b, 255)
+			end
+		end
+	end
 end
 
 
 
-client.set_event_callback("paint",
-    SwastikaCrosshair
+
+client_set_event_callback("paint",
+SwastikaCrosshair
 )
